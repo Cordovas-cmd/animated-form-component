0dddd3cf62aeb4fce35790d44bcff94f0e32a6f7
samuel cordova <cordovas1337@gmail.com>
2022-10-13 00:23:12 -0500

    finished the styles and javascript

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ labels.forEach(label => {
     label.innerHTML = label.innerText
     // splits letters into an array
     .split('')
-    // turn into an array of letters
-    .map((letter, idx) => `<span>${letter}</span>`)
+    // turn into an array of letters and set an inline style for transition delay multiplying based on the index of the current letter.
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
     .join('')
 
 });
